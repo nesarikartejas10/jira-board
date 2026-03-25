087a36c1a1346bfc72be6a7a3ce58278f6798491
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-25 07:00:48 +0530

    handle task input and status selection in TaskForm component

--- src/components/task-form/TaskForm.jsx
+++ src/components/task-form/TaskForm.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import Tag from "../tag/Tag";
 import "./TaskForm.css";
 
 const TaskForm = () => {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "To Do",
+  });
+
+  const handleTaskChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  console.log(taskData);
   return (
     <header className="app_header">
       <form className="task_form">
@@ -9,6 +23,9 @@ const TaskForm = () => {
           type="text"
           className="task_input"
           placeholder="Enter task detail"
+          name="task"
+          value={taskData.task}
+          onChange={handleTaskChange}
         />
 
         <div className="task_form_bottom">
@@ -19,7 +36,12 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              value={taskData.status}
+              onChange={handleTaskChange}
+            >
               <option value="To Do">To Do</option>
               <option value="In Progress">In Progress</option>
               <option value="In Review">In Review</option>
